615c6c1ff97a6fde07964b37d47096f58bc0f1a3
Vasyl Liulka <vasyl.liulka@oxid-esales.com>
2021-06-01 10:55:59 +0200

    OXDEV-4656 Fix grunt paths

--- grunt/less.js
+++ grunt/less.js
@@ -5,19 +5,19 @@ module.exports = {
         yuicompress: true,
         optimization: 2,
         plugins: [
-            new (require('less-plugin-autoprefix'))({browsers: ["last 2 versions"]})
+            new(require('less-plugin-autoprefix'))({browsers: ["last 2 versions"]})
         ]
     },
 
     theme: {
         files: {
-            "out/flow/src/css/styles.min.css": "build/less/flow.less"
+            "out/twig/src/css/styles.min.css": "build/less/flow.less"
         }
     },
 
     vendor: {
         files: {
-            "out/flow/src/css/libs/jquery.flexslider.min.css": "build/vendor/jquery-flexslider2/less/flexslider.less"
+            "out/twig/src/css/libs/jquery.flexslider.min.css": "build/vendor/jquery-flexslider2/less/flexslider.less"
         }
     }
 
